70054527bb2dc21081c8561db6e5b4ffdfa8e45f
kimjustind <kimjustind@gmail.com>
2021-06-05 23:47:42 -0700

    Changed navbar

--- js/darkmode.js
+++ js/darkmode.js
@@ -3,22 +3,24 @@ window.onload = function() {
     dbool = (localStorage.getItem('isDark') === "true");
     if (dbool == true) {
         document.body.classList.add("dark");
-        document.getElementById("darkButton").innerHTML = "<img src='imgs/moon.svg' alt='dark'>";
+        document.getElementById("darkButton").innerHTML = "<img src='imgs/sun.svg' alt='light'>";
     } else {
         document.body.classList.remove("dark");
-        document.getElementById("darkButton").innerHTML = "<img src='imgs/sun.svg' alt='light'>";
+        document.getElementById("darkButton").innerHTML = "<img src='imgs/moon.svg' alt='dark'>";
     }
 };
 
 function darkMode() {
     if (dbool == false) {
         document.body.classList.add("dark");
-        document.getElementById("darkButton").innerHTML = "<img src='imgs/moon.svg' alt='dark'>";
+        document.getElementsByClassName("navbar")[0].classList.add("darknav");
+        document.getElementById("darkButton").innerHTML = "<img src='imgs/sun.svg' alt='light'>";
         localStorage.setItem('isDark', true);
         dbool = true;
     } else {
         document.body.classList.remove("dark");
-        document.getElementById("darkButton").innerHTML = "<img src='imgs/sun.svg' alt='light'>";
+        document.getElementsByClassName("navbar")[0].classList.remove("darknav");
+        document.getElementById("darkButton").innerHTML = "<img src='imgs/moon.svg' alt='dark'>";
         localStorage.setItem('isDark', false);
         dbool = false;
     }
